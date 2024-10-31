7bf6fade5cd59937f2d4e9ad0b2a8f81c6fcb4b2
tobiascarosso <tobicarosso@gmail.com>
2024-10-31 09:18:57 -0300

    a

--- practico10/scripts.js
+++ practico10/scripts.js
@@ -133,4 +133,11 @@ console.log(numeros_duplicados);
 
 //+27
 
-let person
+let personas = [
+    {nombre: "Ana", edad: "28"},
+    {nombre: "Maria", edad: "24"},
+    {nombre: "Jose", edad: "31"}
+];
+
+let personasConMasDe27 = personas.filter(persona => parseInt(persona.edad) > 27);
+console.log(personasConMasDe27);
