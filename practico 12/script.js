79900c72c1b8fd9478bbcc35c9fee4774e0311ad
tobiascarosso <tobicarosso@gmail.com>
2024-10-31 11:13:38 -0300

    a

---
+++ practico 12/script.js
@@ -0,0 +1,21 @@
+const words = [
+    "californication",
+    "plataforma5",
+    "black",
+    "summer",
+    "flea",
+    "aeroplane",
+    "peppers",
+    "unlimited",
+    "arcadium",
+    "love",
+    "getaway",
+    "stadium",
+    "quixoticelixer",
+    "quarter",
+    "snow",
+    "dylan",
+    "zephyr",
+    "funky",
+    "chili"
+    ];  
