8b54081d98c1505dc9d312eb474172469e0eaa81
tobiascarosso <tobicarosso@gmail.com>
2024-10-31 09:54:38 -0300

    a

--- practico10/scripts.js
+++ practico10/scripts.js
@@ -178,3 +178,28 @@ for (let pelicula in actoresPrincipalesPorPelicula) {
     }
     peliculaPorActor[actor].push(pelicula);
 }
+
+
+
+//SOFTWARE FACTORY
+
+function calcularValorTotal(ecommerce) {
+    return ecommerce.map(producto => {
+        return {
+            nombre: producto.nombre,
+            total: producto.precio * producto.articulos
+        };
+    });
+}
+
+let ecommerce = [
+    {nombre: "Samsung TV", precio: 6000, articulos: 10},
+    {nombre: "DELL notebook", precio: 4000, articulos: 30},
+    {nombre: "Auriculares Sony", precio: 1500, articulos: 15},
+    {nombre: "Monitor Philips", precio: 12000, articulos: 50},
+    {nombre: "Teclado logitech", precio: 3000, articulos: 5},
+]
+
+
+let resultado = calcularValorTotal(ecommerce);
+resultado.forEach(item => console.log(`${item.nombre}: ${item.total}`));
